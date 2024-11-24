92e9ce11c8df43d1c9db1e2c191c13e90c68085f
vasimakram-786 <rvasimakram2001@gmail.com>
2024-11-24 14:26:56 +0530

    Delete Java8Features directory

--- Java8Features/DefaultMethods/Company1.java
+++
@@ -1,9 +0,0 @@
-package DefaultMethods;
-
-public interface Company1 
-{
- default void Salary(int a, int b)
- {
-	 System.out.println(a+b);
- }
-}
--- Java8Features/DefaultMethods/Company2.java
+++
@@ -1,9 +0,0 @@
-package DefaultMethods;
-
-public interface Company2 
-{
- default void Salary(int a, int b)
- {
-  System.out.println(a-b);
- }
-}
